e21d513858e1c86e43f0669e4b525b66581cf4b4
luyerabert <41442935+luyerabert@users.noreply.github.com>
2025-10-28 16:52:18 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,28 @@
+name="luye rabert"
+print("name is",name)
+age=25
+print(age)
+height=105.7
+print(height,"cm")
+weight=95
+print(weight,"kg")
+city="madurai"
+print("city is",city)
+is_student=True
+print(is_student)
+mark=99
+print("mark is",mark)
+pi=3.14
+print(pi)
+school_name="de nobili"
+print("my school is",school_name)
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+my_number=10
+print(my_number)
+print(type(my_number))
+my_text="hello"
+print(my_text)
+print(type(my_text))
