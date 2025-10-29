0e67325e99f08d97a8d7e90c6a94fde48f3f06a0
luyerabert <41442935+luyerabert@users.noreply.github.com>
2025-10-29 17:49:04 +0530

    Delete tot&avg.py

--- tot&avg.py
+++
@@ -1,8 +0,0 @@
-a=int(input("enter 1st subject"))
-b=int(input("enter 2nd subject"))
-c=int(input("enter 3rd subject"))
-d=int(input("enter 4th subject"))
-e=int(input("enter 5th subject"))
-f=a+b+c+d+e
-print(f)
-print(f/5)
